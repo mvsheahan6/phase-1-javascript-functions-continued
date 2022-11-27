2bca2f26a7083177243b61034da608dda912fa3a
Matthew Sheahan <sheahan.matt@gmail.com>
2022-11-27 15:05:20 -0800

    commit tree b770ad0fd7915f600d6beb929c2e7e476f33cc44

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+// function saturdayFun (activity = "roller-skate") {
+//     console.log `This Saturday I want to ${activity}`
+// }
+
+function saturdayFun(funThing="roller-skate") {
+  return `This Saturday, I want to ${funThing}!`
+};
+
+saturdayFun("bathe my dog");
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+};
+
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+  return function(inn = "special") {
+    return `You are ${flair}${inn}${flair}!`
+  }
+}
+
